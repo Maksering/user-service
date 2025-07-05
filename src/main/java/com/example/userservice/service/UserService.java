11aefb5af60b2784f070d9bf872654b997b2a06e
Maksering <maksering.off@gmail.com>
2025-07-06 01:01:28 +0300

    add circuit breaker to service

--- src/main/java/com/example/userservice/service/UserService.java
+++ src/main/java/com/example/userservice/service/UserService.java
@@ -7,6 +7,7 @@ import com.example.userservice.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,63 +23,104 @@ public class UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final KafkaProducerService kafkaProducer;
-    private final KafkaProducerService kafkaProducerService;
+    private final CircuitBreakerFactory circuitBreakerFactory;
 
     @Transactional
     public List<UserDTO> getAllUsers() {
-        logger.debug("Getting the list of all users");
-        List<UserDTO> users = userRepository.findAll().stream()
-                .map(userMapper::mapToUserDTO)
-                .sorted(Comparator.comparing(UserDTO::getId))
-                .collect(Collectors.toList());
+        logger.debug("Getting list of all users");
+        List<UserDTO> users = circuitBreakerFactory.create("user-service")
+                .run(() ->{
+                    return userRepository.findAll().stream()
+                            .map(userMapper::mapToUserDTO)
+                            .sorted(Comparator.comparing(UserDTO::getId))
+                            .collect(Collectors.toList());
+                }, throwable -> {
+                    logger.error("Fail on getAllUsers");
+                    return List.of();
+                });
         logger.info("Get " + users.size() + " users");
         return users;
     }
 
+
     @Transactional
     public UserDTO getUserById(Long id) {
-        logger.debug("Getting the user by id");
-        return userRepository.findById(id)
-                .map(userMapper::mapToUserDTO)
-                .orElseThrow(() -> {
-                    logger.error("User not found ID:" + id);
-                    return new RuntimeException("User not found by id: " + id);
-                });
+        logger.debug("Getting user by id: " + id);
+
+        return (UserDTO) circuitBreakerFactory.create("user-service")
+                .run(() -> userRepository.findById(id)
+                                .map(userMapper::mapToUserDTO)
+                                .orElseThrow(() -> {
+                                    logger.error("User not found by id: " +  id);
+                                    return new RuntimeException("User not found by id: " + id);
+                                }),
+                        throwable -> {
+                            logger.error("Failed to get user, ID: " +  id);
+                            return  new RuntimeException("User service is unavailable");
+                        });
     }
 
     @Transactional
     public UserDTO createUser(UserDTO userDTO) {
-        logger.debug("Creating new user");
-        User user = userMapper.mapToUserEntity(userDTO);
-        kafkaProducer.sendUserCreate(userDTO.getEmail());
-        logger.info("User created ID: " + user.getId());
-        return userMapper.mapToUserDTO(userRepository.save(user));
+        logger.debug("Creating user: " + userDTO);
+
+        return circuitBreakerFactory.create("user-service")
+                .run(() -> {
+                            User user = userMapper.mapToUserEntity(userDTO);
+                            User savedUser = userRepository.save(user);
+                            kafkaProducer.sendUserCreate(userDTO.getEmail());
+                            logger.info("User created ID: " + savedUser.getId());
+                            return userMapper.mapToUserDTO(savedUser);
+                        },
+                        throwable -> {
+                            logger.error("Failed to create user " + userDTO);
+                            throw new RuntimeException("User service is unavailable");
+                        });
     }
 
     @Transactional
     public UserDTO updateUser(Long id, UserDTO userDTO) {
-        logger.debug("Updating user ID: " + id);
-        User foundedUser = userRepository.findById(id)
-                .orElseThrow(() -> {
-                    logger.error("User not found ID: " + id);
-                    return new RuntimeException("User not found by id: " + id);
-                });
-        userMapper.updateUserFromDTO(userDTO, foundedUser);
-        logger.info("User updated ID: " + id);
-        return userMapper.mapToUserDTO(userRepository.save(foundedUser));
+        logger.debug("Update user ID: " + id);
+
+        return circuitBreakerFactory.create("userServiceCB")
+                .run(() -> {
+                            User existingUser = userRepository.findById(id)
+                                    .orElseThrow(() -> {
+                                        logger.error("User not found by id: "+ id);
+                                        return new RuntimeException("User not found by id: " + id);
+                                    });
+
+                            userMapper.updateUserFromDTO(userDTO, existingUser);
+                            User updatedUser = userRepository.save(existingUser);
+                            logger.info("User updated ID: {}", id);
+                            return userMapper.mapToUserDTO(updatedUser);
+                        },
+                        throwable -> {
+                            logger.error("Failed to update user ID: " + id, throwable);
+                            throw new RuntimeException("User service is unavailable");
+                        });
     }
 
     @Transactional
     public void deleteUser(Long id) {
         logger.debug("Deleting user ID: " + id);
-        User found = userRepository.findById(id).orElseThrow(() -> {
-            logger.error("User not found ID: " + id);
-            return new RuntimeException("User not found by id: " + id);
-        });
-        if (found != null) {
-            userRepository.delete(found);
-            kafkaProducerService.sendUserDelete(found.getEmail());
-            logger.info("User deleted ID: " + id);
-        }
+
+        circuitBreakerFactory.create("userServiceCB")
+                .run(() -> {
+                            User user = userRepository.findById(id)
+                                    .orElseThrow(() -> {
+                                        logger.error("User not found by id: " + id);
+                                        return new RuntimeException("User not found by id: " + id);
+                                    });
+
+                            userRepository.delete(user);
+                            kafkaProducer.sendUserDelete(user.getEmail());
+                            logger.info("User deleted ID: {}", id);
+                            return null;
+                        },
+                        throwable -> {
+                            logger.error("Failed to delete user by id " + id, throwable);
+                            throw new RuntimeException("User service is unavailable");
+                        });
     }
 }
