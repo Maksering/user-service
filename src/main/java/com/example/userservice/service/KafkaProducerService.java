c73f8f84689d35bea454e1b6dfcf4bfa551e90c2
Maksering <maksering.off@gmail.com>
2025-07-11 23:58:26 +0300

    Uncommented KafkaProducerService add DockerFile for dockercompose

--- src/main/java/com/example/userservice/service/KafkaProducerService.java
+++ src/main/java/com/example/userservice/service/KafkaProducerService.java
@@ -1,10 +1,13 @@
 package com.example.userservice.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @Service
 public class KafkaProducerService {
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     public static final String CREATE_OPERATION = "create";
     static final String DELETE_OPERATION = "delete";
@@ -15,14 +18,16 @@ public class KafkaProducerService {
     }
 
     public void sendUserCreate(String email) {
-       /* kafkaTemplate.send(kafkaTemplate.getDefaultTopic(),
+        kafkaTemplate.send(kafkaTemplate.getDefaultTopic(),
                 CREATE_OPERATION,
-                email);*/
+                email);
+        logger.info("Create kafka message was sent");
     }
 
     public void sendUserDelete(String email) {
-      /* kafkaTemplate.send(kafkaTemplate.getDefaultTopic(),
+       kafkaTemplate.send(kafkaTemplate.getDefaultTopic(),
                 DELETE_OPERATION,
-                email);*/
+                email);
+        logger.info("Delete kafka message was sent");
     }
 }
